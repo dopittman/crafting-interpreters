d7f3a236cc5c7b8991b06585e2fdfbca6ce7cec7
David Pittman <53283971+dopittman@users.noreply.github.com>
2023-06-23 20:08:34 -0400

    Feature: add additional Scanner capabilities

--- jlox/com/craftinginterpreters/lox/Lox.java
+++ jlox/com/craftinginterpreters/lox/Lox.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class Lox {
 
     static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -18,7 +19,8 @@ public class Lox {
             runFile(args[0]);
         } else {
             runPrompt();
-        } }
+        }
+    }
 
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
@@ -31,7 +33,7 @@ public class Lox {
     private static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
-        for (;;) {
+        for (; ; ) {
             System.out.print("> ");
             String line = reader.readLine();
             if (line == null) break;
@@ -53,7 +55,9 @@ public class Lox {
     static void error(int line, String message) {
         report(line, "", message);
     }
+
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
+}
